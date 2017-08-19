b4679381d21af8d57dbcae1e57cbbd0a8fd1a885
liukai6 <liu163p@gmail.com>
2017-08-19 21:19:45 +0800

    zheshi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 93
+                    码云
                 </div>
 
                 <div class="links">
